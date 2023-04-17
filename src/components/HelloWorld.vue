7ec50c8dd10a62878b8042ba860dbd26f305f158
Martin Bock <martinbock@macair.local>
2023-04-17 19:08:08 +0200

    init commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="hello-container">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.hello-container {
+  & > h1 {
+    color: red;
+  }
+}
+</style>
